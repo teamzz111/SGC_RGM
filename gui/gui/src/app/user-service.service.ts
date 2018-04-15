da21e67edaf11f458141411c3c4919afa0b529e4
teamzz111 <aflr@outlook.com>
2018-04-15 15:27:47 -0500

    Some fixes (FRONT-END)
    With opinion of our developers we changed some visual parts.

--- gui/gui/src/app/user-service.service.ts
+++ gui/gui/src/app/user-service.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { HttpModule } from '@angular/http';
-
+@Injectable()
 export class UserServiceService {
   constructor(private http: Http) {}
   listar() {
     return this.http.get(
-      'http://localhost/sgc/gui/php/test.php?srv=1'
-    );
+      'http://localhost/sgc/gui/php/test.php?srv=1');
   }
   closeSession() {
     return this.http.get('http://localhost/sgc/gui/php/test.php?srv=2');
