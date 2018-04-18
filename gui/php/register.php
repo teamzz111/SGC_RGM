8b54f1908a27934e1fb1b2d4edfd6310b13cd2e2
teamzz111 <aflr@outlook.com>
2018-04-18 18:34:39 -0500

    IntelliJ IDE settings and little changes on PHP

--- gui/php/register.php
+++ gui/php/register.php
@@ -1,14 +1,36 @@
 <?php
-    include 'test.php';
-    if(isset($_GET['opt']) && $_GET['opt'] == 1){
+    $host = "localhost";
+    $db = "bd";
+    $pass = "";
+    if(isset($_GET['opt'])) {
         $con = new mysqli($host, "root", $pass, $db);
-        if($con -> connect_error){
+        if ($con->connect_error) {
             die("Conexión errónea: " . $con->connect_error);
         }
         $rs;
-        $query = "SELECT seccional.idSeccional, cargo.idCargo  FROM seccional, cargo;
+        $query;
 
+        if( $_GET['opt'] == 1 ) {
+            $query = "SELECT nombre, nivel FROM cargo";
+        }
+        else{
+            $query = "SELECT seccional.ciudad, seccional.pais FROM seccional";
+        }
+        $rs = $con->query($query);
+        if ($rs) {
+
+            $array = array();
+            while ($fila = mysqli_fetch_assoc($rs)) {
+    
+                $array[] = array_map('utf8_encode', $fila);
+            }
+
+            $res = json_encode($array, JSON_NUMERIC_CHECK);
+        } else {
+            $res = null;
+            echo mysqli_error($con);
+        }
+        mysqli_close($con);
+        echo $res;
     }
-
-
 ?>
