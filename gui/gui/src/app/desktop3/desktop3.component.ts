9bf6bdfcf058f6378769d97e6b743925c051d0b4
Jorge Morales <jamorales516@icloud.com>
2018-04-19 13:34:46 -0500

    Frgt psswd 2.0
    bugs fixed, new HTML document created, some other advances

--- gui/gui/src/app/desktop3/desktop3.component.ts
+++ gui/gui/src/app/desktop3/desktop3.component.ts
@@ -12,7 +12,7 @@ export class Desktop3Component  {
   semidesk: number;
   constructor(private crudProducto: UserServiceService) {
          this.nombre = 'Andrés Largo';
-         this.semidesk = 0;
+         this.semidesk = 3;
   }
 
 
--- gui/php/register.php
+++ gui/php/register.php
@@ -1,10 +1,11 @@
 <?php
    // include 'test.php';
-    if(isset($_GET['opt'])){
-
-    } else {
-
-        if(isset($_POST['cedula']) && isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['correo']) && isset($_POST['telefono']) && isset($_POST['direccion'])&& isset($_POST['numero'])/**/)
+    $db = "bd";
+    $host = "localhost";
+    $pw = "";
+    $user = "root";
+    if(!isset($_GET['opt'])) {
+        if (isset($_POST['cedula']) && isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['correo']) && isset($_POST['telefono']) && isset($_POST['direccion']) && isset($_POST['numero'])/**/) 
         {
             $Cedula = $_POST['cedula'];
             $Nombre = $_POST['nombre'];
@@ -13,27 +14,60 @@
             $Telefono = $_POST['telefono'];
             $Direccion = $_POST['direccion'];
             $Numero = $_POST['numero'];
+            echo $_POST['cedula'];
 
-            $db = "bd";
-            $host = "localhost";
-            $pw = "";
-            $user = "root";
-            $con = mysql_connect($host,$user,$pw) or die("No se pudo conectar con la bd. ");
-            echo '<script language="javascript">alert("todo bien xd");</script>'; 
-            mysql_select_db($db,$con) or die("No se pudo conectar a la base de datos. ");
-            mysql_query("INSERT INTO empleado(`cedula`, `nombre`, `apellido`, `email`, `telefono`, `direccion`, `numero`, `sexo`, `idSeccional`, `idCargos`) VALUES
-            ('$Cedula', '$Nombre', '$Apellido', '$Correo', '$Telefono', '$Direccion', '$Numero', 'm', 1,0)");
+            $con = new mysqli($host, "root", $pw, $db);
+            if ($con->connect_error) {
+                die("Conexión errónea: " . $con->connect_error);
+            }
+    
+            if($Cedula>=1)
+            {
+                echo "eso ya existe ome";// Inserte aquí la acción a realizar en caso de que la cédula digitada ya esté registrada
+            }else
+            {
+                $query = "INSERT INTO `empleado` VALUES ('$Cedula', '$Nombre', '$Apellido', '$Correo', '$Telefono', '$Direccion', '$Numero', 'm', 1,1)";
+                $rs = $con->query($query);
+                echo $query;
+            if ($rs) {
+                echo json_encode('true');
+            } else {
+                echo json_encode('false');
+            }
+        } 
+
+        }else {
+            echo json_encode('0');
+        }
             
-            echo '<script language="javascript">alert("Empleaducho añadido");</script>'; 
-        }
-        else   
-        {
-            echo "llene el campo gonorrea ome gonorrea";
-        }
-      
     }
-
+    else{
+        if($_GET['opt'] == 1){
+            $query = "SELECT nombre, nivel FROM cargo";
+            $con = new mysqli($host, "root", $pw, $db);
+            $rs = $con->query($query);
+            $array = array();
+            $count = 0;
+            if ($rs) {
+                $array = array();
+                while ($fila = mysqli_fetch_assoc($rs)) {
+                    $count++;
+                    $array[] = array_map('utf8_encode', $fila);
+                }
+                if($count == 0 ) {
+                    echo json_encode('error');
+                    exit(0);
+                }
+                $res = json_encode($array, JSON_NUMERIC_CHECK);
+            }else{
+                $res = null;
+                echo mysqli_error($con);
+            }
+            mysqli_close($con);
+            echo $res;
+        }
+    }
 
     
-    }
+    
 ?>
