468b8dea649ee530af6e52e5c3fb194479dd04d8
teamzz111 <aflr@outlook.com>
2018-04-19 02:20:10 -0500

    Php -Register.php has been improved

--- gui/php/register.php
+++ gui/php/register.php
@@ -1,8 +1,7 @@
 <?php
    // include 'test.php';
-
-        if(isset($_POST['cedula']) && isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['correo']) && isset($_POST['telefono']) && isset($_POST['direccion'])&& isset($_POST['numero'])/**/)
-        {
+    if(!isset($_GET['opt'])) {
+        if (isset($_POST['cedula']) && isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['correo']) && isset($_POST['telefono']) && isset($_POST['direccion']) && isset($_POST['numero'])/**/) {
             $Cedula = $_POST['cedula'];
             $Nombre = $_POST['nombre'];
             $Apellido = $_POST['apellido'];
@@ -16,29 +15,28 @@
             $pw = "";
             $user = "root";
             $con = new mysqli($host, "root", $pw, $db);
-            if($con -> connect_error){
+            if ($con->connect_error) {
                 die("Conexión errónea: " . $con->connect_error);
             }
-        
-$query = "INSERT INTO `empleado` VALUES ('$Cedula', '$Nombre', '$Apellido', '$Correo', '$Telefono', '$Direccion', '$Numero', 'm', 1,0)";  
+
+            $query = "INSERT INTO `empleado` VALUES ('$Cedula', '$Nombre', '$Apellido', '$Correo', '$Telefono', '$Direccion', '$Numero', 'm', 1,0)";
             $rs = $con->query($query);
             echo $query;
-                   if($rs){
-            echo '<script language="javascript">alert("Empleaducho añadido");</script>'; 
-                   }
-                   else
-                {
-                    echo $con->error;
-                    echo 'hola';
-                }
-        }
-        else   
-        {
+            if ($rs) {
+                echo '<script language="javascript">alert("Empleaducho añadido");</script>';
+            } else {
+                echo $con->error;
+                echo 'hola';
+            }
+        } else {
             echo "llene el campo gonorrea ome gonorrea";
         }
-      
-    
-
+    }
+    else{
+        if($_GET['opt'] == 1){
+            
+        }
+    }
 
     
     
