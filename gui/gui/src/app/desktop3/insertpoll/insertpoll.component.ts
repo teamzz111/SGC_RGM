81538244a4e24da2b0901e52ec7014ebcec25371
teamzz111 <aflr@outlook.com>
2018-05-11 01:07:22 -0500

    First part of poll system

--- gui/gui/src/app/desktop3/insertpoll/insertpoll.component.ts
+++ gui/gui/src/app/desktop3/insertpoll/insertpoll.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var jquery: any;
+declare var $: any;
 @Component({
   selector: 'app-insertpoll',
   templateUrl: './insertpoll.component.html',
   styleUrls: ['./insertpoll.component.css']
 })
-export class InsertpollComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class InsertpollComponent {
+  next: number;
+  tipo: number;
+  constructor() {
+    this.next = 0;
   }
-
+  prueab() {
+  this.tipo = $('#tipo').prop('selectedIndex');  }
 }
