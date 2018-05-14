fc3b086d0fbc1d4de632c0b85c06203a371c3bf1
MonoAncestral <37392918+MonoAncestral@users.noreply.github.com>
2018-05-14 17:50:14 -0500

    Add poll

--- gui/gui/php/Addpoll.php
+++ gui/gui/php/Addpoll.php
@@ -1,18 +1,39 @@
 <?php
-    if(isset($_GET['opt'])){
+
+include 'Conexion.php';
+
+if(!isset($_GET['opt'])) {
         $inputJSON = file_get_contents('php://input');
-        $result = json_decode($inputJSON, true);
-        if($_GET['opt'] == 1){ // se registra encuesta
-            echo json_encode($result['nombre']);
-            
-        } else if ($_GET['opt'] == 2){ //se registra pregunta
-             if($result['nrespuesta'] == 0){ // es una respuesta abierta 
-                 echo json_encode($result['pregunta']);
-             }
-            else { //es una pregunta con múltiple opción
-                echo json_encode($result['r1']);
-            }
+        $input = json_decode($inputJSON, TRUE);
+        $Nombre = $input['nombre'];
+        $Tipo= $input['tipo'];
+
+        $query = "SELECT Id FROM Tipo WHERE Nombre = '$Tipo'";
+        $resultado = $con->query($query);
+        $row = $resultado->fetch_array(MYSQLI_ASSOC);
+        $Tipo = $row['idCargos'];
+
+        $con = new mysqli($host, $user, $pass, $db);
+        $con->query("SET NAMES 'utf8'");
+        if ($con->connect_error) {
+            echo json_encode('falseC');
         }
-        
-    }
+         $char='ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+            $long=strlen($char)-1;
+            $j[0]=0;
+            for($i=0;$i<6;$i++){
+                $c=rand(0,$long);
+                $n=$char[$c];
+                $j[0].=$n;
+            }
+            $crp = $j[0];
+            $query = "INSERT INTO seccional VALUES ('CONTRASEÑA IRÁ AQUI','$Nombre',$Tipo)";//luego sigo, tengo sueño xd
+            $rs = $con->query($query);
+            if ($rs) {
+                echo json_encode('true');
+                }
+                else {
+                    echo json_encode('false');
+                }
+        }
 ?>
