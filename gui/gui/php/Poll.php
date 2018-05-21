224a8012b2cbe413b8d8313da163607abb64a811
MonoAncestral <37392918+MonoAncestral@users.noreply.github.com>
2018-05-20 19:04:30 -0500

    Questions
    Co-Authored-By: Andrés Largo <contact@andreslargo.com>

--- gui/gui/php/Poll.php
+++ gui/gui/php/Poll.php
@@ -2,7 +2,10 @@
 
 include 'Conexion.php';
 
-if(isset($_GET['opt'])) {
+if(isset($_GET['opt'] == 1) {   
+    
+    $inputJSON = file_get_contents('php://input');
+    $result = json_decode($inputJSON, true);
     $con = new mysqli($host, $user, $pass, $db);
     $con->query("SET NAMES 'utf8'");
     $Fecha= $input['fecha'];
@@ -31,6 +34,35 @@ if(isset($_GET['opt'])) {
             mysqli_close($con);
             echo $res;
 
+    }
+    else if ($_GET['opt'] == 2)
+    {
+        $inputJSON = file_get_contents('php://input');
+        $result = json_decode($inputJSON, true);
+        $con = new mysqli($host, $user, $pass, $db);
+        $con->query("SET NAMES 'utf8'");
+        $Id= $input['id'];
+        
+        $query = "SELECT Pregunta, Numero, Respuesta1, Respuesta2, Respuesta3, Respuesta4, Respuesta5  FROM Pregunta     WHERE idEncuesta='$Id'";
+        $resultado = $con->query($query);
+        if ($resultado)
+        {
+            if($resultado->num_rows >0)
+            {
+                $array = array();
+                while ($fila = mysqli_fetch_assoc($resultado)) {
+                    $array[] = array_map('html_entity_decode', $fila);
+                }
+                $res = json_encode($array, JSON_UNESCAPED_UNICODE);
+            }
+            else
+            {
+                $res=null;
+                echo json_encode("nel");
+            }
+        }
+        mysqli_close($con);
+        echo $res;
     }
 
 
