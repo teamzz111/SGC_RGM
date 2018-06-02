b316fec30b8dafeb65f0c3ab9228a1b38a62bc41
teamzz111 <aflr@outlook.com>
2018-06-01 22:54:03 -0500

    Arreglo de rutas

--- login/forgotpsswd.php
+++ login/forgotpsswd.php
@@ -64,7 +64,7 @@
 				//	$mail->addBCC('bcc@example.com');
 				//	$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 				///	$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-				$mail->AddEmbeddedImage('../../img/sgc.png', 'logo_2u');
+				$mail->AddEmbeddedImage('../dist/assets/img/sgc.png', 'logo_2u');
 				//Content
 				$mail->isHTML(true);                                  // Set email format to HTML
 				$mail->Subject = '¿OLVIDASTE TU CONTRASEÑA?';
