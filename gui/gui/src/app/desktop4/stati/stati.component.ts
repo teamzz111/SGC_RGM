202659b13b5a9ba7d1731a4e694ba8ee8d9e4af3
teamzz111 <aflr@outlook.com>
2018-06-02 23:41:50 -0500

    Parte front de estadísticas finales completada

--- gui/gui/src/app/desktop4/stati/stati.component.ts
+++ gui/gui/src/app/desktop4/stati/stati.component.ts
@@ -1,5 +1,6 @@
 import { UserServiceService } from './../../user-service.service';
 import { Component, OnInit  } from '@angular/core';
+import {Chart} from 'chart.js';
 declare var jquery: any;
 declare var $: any;
 import 'rxjs/add/operator/map';
@@ -8,13 +9,63 @@ import 'rxjs/add/operator/map';
   templateUrl: './stati.component.html',
   styleUrls: ['./stati.component.css']
 })
-export class StatiComponent implements OnInit{
+export class StatiComponent implements OnInit {
   public idEncuesta = [];
   public porcentajes = [];
   public listado;
   public max: number;
+  public lugar = true;
+  public gra = false;
+  public encuesta;
   constructor(private crudProducto: UserServiceService) {
     this.max = 0;
+  }
+  go(index) {
+    this.lugar = true;
+    this.encuesta = this.idEncuesta[index];
+  }
+
+  loadGraphics() {
+    if (!this.gra) {
+      this.gra = true;
+      $('#inservible').remove();
+      const myChart = new Chart($('#myChart')[0].getContext('2d'), {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      });
+    }
   }
   ready() {
       $('.test-circle').circliful({
@@ -28,20 +79,18 @@ export class StatiComponent implements OnInit{
       this.max = 0;
     }
   ngOnInit() {
-    this.crudProducto.verConteoEncuesta()
+    /*this.crudProducto.verConteoEncuesta()
     .map(response => response.json())
     .subscribe(data => {
       this.listado = data;
       for (const i of this.listado) {
         this.idEncuesta.push(i.idEncuesta);
         this.max++;
-        let percent = (i.Respondido * 100) / (i.Faltantes);
+        const percent = (i.Respondido * 100) / (i.Faltantes);
         this.porcentajes.push(percent);
       }
-    });
+    });*/
 
+   
   }
-
-  
-
 }
