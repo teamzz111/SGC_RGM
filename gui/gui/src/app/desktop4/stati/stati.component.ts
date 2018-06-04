3c99017c684bf5f7597c4c3c2c67b5a9b8f7201d
teamzz111 <aflr@outlook.com>
2018-06-04 02:56:45 -0500

    Terminando estadísticas

--- gui/gui/src/app/desktop4/stati/stati.component.ts
+++ gui/gui/src/app/desktop4/stati/stati.component.ts
@@ -1,4 +1,3 @@
-import { Pregunta } from './../../desktop5/pregunta';
 import { UserServiceService } from './../../user-service.service';
 import { Component, OnInit  } from '@angular/core';
 import {Chart} from 'chart.js';
@@ -19,76 +18,105 @@ export class StatiComponent implements OnInit {
   public lugar = false;
   public gra = false;
   public encuesta;
+  max2;
+  lista;
+  idspreguntas = [];
   constructor(private crudProducto: UserServiceService) {
     this.max = 0;
+    this.max2 = 0;
   }
   go(index) {
     this.lugar = true;
     this.encuesta = this.idEncuesta[index];
+    this.crudProducto.estadisticasEncuesta(JSON.stringify({ id: this.encuesta }))
+      .map(response => response.json())
+      .subscribe(data => {
+        if (data === 'sinrespuestas') {
+          this.lugar = false;
+          $('.notifi').css({ background: 'red' });
+          $('.notifi').text('Sin preguntas...');
+          $('.notifi').animate({ marginTop: '2em' }, 1000, function () {
+            setTimeout(function () {
+              $('.notifi').animate({ marginTop: '-10em' }, 1000); }, 3000);
+
+          });
+        } else {
+        this.lista = data;
+        for (const i of data) {
+          if (i.Pregunta !== '') {
+            this.preguntas.push(i.Pregunta);
+            this.idspreguntas.push(i.idPregunta);
+            this.max2++;
+          }
+        }
+      }
+      });
   }
 
   loadGraphics() {
     if (!this.gra) {
       this.gra = true;
       $('#inservible').remove();
-      this.crudProducto.estadisticasEncuesta(JSON.stringify({ id: this.encuesta}))
-        .map(response => response.json())
-        .subscribe(data => {
-          console.log(data);
-          for (const i of data) {
-            if (i.Pregunta !== '') {
-              this.preguntas.push(i.Pregunta);
-            }
-            if (i[0].Pregunta2 !== '') {
-              this.preguntas.push(i[0].Pregunta);
-            }
-            if (i[1].Pregunta3 !== '') {
-              this.preguntas.push(i[1].Pregunta);
-            }
-            if (i[2].Pregunta4 !== '') {
-              this.preguntas.push(i[2].Pregunta);
-            }
-            if (i.Pregunta4 !== '') {
-              this.preguntas.push(i.Pregunta);
-            }
-          const myChart = new Chart($('#myChart')[0].getContext('2d'), {
-            type: 'bar',
-            data: {
-              labels: this.preguntas,
-              datasets: [{
-                label: i.Nombre,
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-              }]
-            },
-            options: {
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }]
-              }
-            }
-          });
+      for (const i of this.lista) {
+        const respu = [];
+        const cont = [];
+        if (i.Respuesta1 !== '') {
+          respu.push(i.Respuesta1);
+          cont.push(i.Contador1);
         }
-        });
+        if (i.Respuesta2 !== '') {
+          respu.push(i.Respuesta2);
+          cont.push(i.Contador2);
+        }
+        if (i.Respuesta3 !== '') {
+          respu.push(i.Respuesta3);
+          cont.push(i.Contador3);
+        }
+        if (i.Respuesta4 !== '') {
+          respu.push(i.Respuesta4);
+          cont.push(i.Contador4);
+        }
+        if (i.Respuesta5 !== '') {
+          respu.push(i.Respuesta5);
+          cont.push(i.Contador5);
+        }
+        const myChart = new Chart($('#' + i.idPregunta)[0].getContext('2d'), {
+                type: 'bar',
+                data: {
+                  labels: respu,
+                  datasets: [{
+                    label: i.Pregunta,
+                    data: cont,
+                    backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                  }]
+                },
+                options: {
+                  scales: {
+                    yAxes: [{
+                      ticks: {
+                        beginAtZero: true,
+                      }
+                    }]
+                  }
+                },
+              });
+            }
     }
   }
   ready() {
@@ -114,6 +142,6 @@ export class StatiComponent implements OnInit {
         this.porcentajes.push(percent);
       }
     });
-   
+
   }
 }
