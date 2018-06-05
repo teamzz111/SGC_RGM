4ba7f3952e59eabde7ac156606f2d36308e5b1f9
teamzz111 <aflr@outlook.com>
2018-06-04 19:54:03 -0500

    Correciones en documentos

--- gui/gui/src/assets/curso/index.php
+++ gui/gui/src/assets/curso/index.php
@@ -7,10 +7,67 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/bootstrap-theme.css" rel="stylesheet">
-		<link href="css/jquery.dataTables.min.css" rel="stylesheet">
+		<link href="css/datatables.min.css" rel="stylesheet">
+
+		
+	</head>
+	
+	<body>
+		
+		<div class="container">
+			<div class="row">
+				<h2 style="text-align:center">SGC - GESTION DE ARCHIVOS</h2>
+			</div>
+			<div class="row" style = "width: 100%; text-align: center;">
+				<a href="nuevo.php" class="btn btn-primary">Nuevo Registro</a>
+			</div>
+			
+			<br>
+					<div class="row table-responsive">
+				<table class="display" id="mitabla">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Nombre</th>
+							<th>Email</th>
+							<th>Telefono</th>
+							<th></th>
+							<th></th>
+						</tr>
+					</thead>
+					
+					<tbody>
+						
+					</tbody>
+				</table>
+			</div>	
+		
+		</div>
+		
+		<!-- Modal -->
+		<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h4 class="modal-title" id="myModalLabel">Eliminar Registro</h4>
+					</div>
+					
+					<div class="modal-body">
+						¿Desea eliminar este registro?
+					</div>
+					
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+						<a class="btn btn-danger btn-ok">Delete</a>
+					</div>
+				</div>
+			</div>
+		</div>
 		<script src="js/jquery-3.1.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>	
-		<script src="js/jquery.dataTables.min.js"></script>
+		<script src="js/datatables.min.js"></script>
 		
 		<script>
 			$(document).ready(function(){
@@ -37,46 +94,6 @@
 			});
 			
 		</script>
-		
-	</head>
-	
-	<body>
-		
-		<div class="container">
-			<div class="row">
-				<h2 style="text-align:center">SGC - GESTION DE ARCHIVOS</h2>
-			</div>
-			<div class="row" style = "width: 100%; text-align: center;">
-				<a href="nuevo.php" class="btn btn-primary">Nuevo Registro</a>
-			</div>
-			
-			<br>
-			
-		
-		</div>
-		
-		<!-- Modal -->
-		<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h4 class="modal-title" id="myModalLabel">Eliminar Registro</h4>
-					</div>
-					
-					<div class="modal-body">
-						¿Desea eliminar este registro?
-					</div>
-					
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-						<a class="btn btn-danger btn-ok">Delete</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		
 		<script>
 			$('#confirm-delete').on('show.bs.modal', function(e) {
 				$(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
