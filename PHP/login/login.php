bbea58839e6ef61a56c285ee02bb95b3270cf4ac
teamzz111 <felipeflogxd@gmail.com>
2019-02-09 20:16:37 -0500

    Updating login method

--- PHP/login/login.php
+++ PHP/login/login.php
@@ -12,18 +12,17 @@
     if($_GET['auth'] == 1){
         $user = $_POST['user'];
         $pass = $_POST['pass'];
-        $query = "SELECT cuenta.contrasena, empleado.cargo_idCargos, cargo.nivel FROM cuenta, empleado, cargo WHERE cuenta.cedula = $user AND empleado.cedula = $user AND empleado.cargo_idCargos = cargo.idCargos";
+        $query = "SELECT cuenta.contrasena, empleado.cargo_idCargos, cargo.nivel FROM cuenta, empleado, cargo WHERE cuenta.cedula = $user AND empleado.cedula = $user AND empleado.cargo_idCargos = cargo.idCargos AND cuenta.contrasena = ". encrypt($pass, $key);
 
         $result = $con->query($query);
 
         if($result->num_rows > 0){
             $row = $result ->fetch_array(MYSQLI_ASSOC);
-            if (encrypt($pass, $key) == $row['contrasena']){
                 $_SESSION['loggedin'] = true;
                 $_SESSION['username'] = $user;
                 $_SESSION['job'] = $row['nivel'];
                 echo "true";
-            }
+            
             else{
                 echo "false";
             }
